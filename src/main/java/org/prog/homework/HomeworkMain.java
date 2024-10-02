4c35a21c07c4c92bde55e3da36bf536157f701fa
dkhorkova <darynakhorkova@gmail.com>
2024-10-02 16:37:43 +0200

    homework2 Daryna Khorkova

--- src/main/java/org/prog/homework/HomeworkMain.java
+++ src/main/java/org/prog/homework/HomeworkMain.java
@@ -1,8 +1,31 @@
 package org.prog.homework;
 
+
 public class HomeworkMain {
 
     public static void main(String[] args) {
+        String destination = "Milan";
+        String startAirport = "Odessa";
+        String transitAirport = "Stanbul";
+        String nameOfCompany = "Luftansa";
+        int flightNumber = 650;
+
+
+        flyTo(destination, startAirport, transitAirport);
+
 
     }
+    public static void flyTo ( String destination, String startAirport, String transitAirport) {
+   }
+   public static void flyTo ( String destination, String startAirport, String transitAirport, String nameOfCompany,
+                              int flightNumber){
+        System.out.println ( " Flight " + flightNumber + "of" + nameOfCompany + " from " + startAirport + " to " +
+                destination + " through " + transitAirport);
+   }
 }
+
+
+
+
+
+
