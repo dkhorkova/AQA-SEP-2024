f557dbf31a5fb3f73ebc0dda86980b1eefc06322
aosipenko <aoskiev@gmail.com>
2024-10-25 19:47:52 +0200

    Session 11: docker network

---
+++ src/main/java/org/prog/util/WebDriverFactory.java
@@ -0,0 +1,44 @@
+package org.prog.util;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+
+public class WebDriverFactory {
+
+    public static WebDriver getDriver() throws MalformedURLException {
+        //LOCAL - Intellij
+        //SELENOID - Intellij + Selenoid
+        //JENKINS - Jenkins + Selenoid
+        String env = System.getProperty("env", "LOCAL");
+        switch (env) {
+            case "LOCAL":
+                return new ChromeDriver();
+            case "SELENOID":
+                return new RemoteWebDriver(
+                        new URL("http://localhost:4444/wd/hub"), remoteChrome());
+            case "JENKINS":
+                return new RemoteWebDriver(
+                        new URL("http://selenoid-selenoid-1:4444/wd/hub"), remoteChrome());
+            default:
+                throw new RuntimeException("No env defined!");
+        }
+
+    }
+
+    private static ChromeOptions remoteChrome() {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.setAcceptInsecureCerts(true);
+        chromeOptions.addArguments("--remote-allow-origins=*");
+        chromeOptions.setCapability("selenoid:options", new HashMap<String, Object>() {{
+            put("enableVideo", true);
+            put("enableVNC", true);
+        }});
+        return chromeOptions;
+    }
+}
--- src/test/java/org/prog/cucumber/CucumberAllTestRunner.java
+++ src/test/java/org/prog/cucumber/CucumberAllTestRunner.java
@@ -2,15 +2,12 @@ package org.prog.cucumber;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.RemoteWebDriver;
 import org.prog.cucumber.steps.WebSteps;
+import org.prog.util.WebDriverFactory;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
 import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.HashMap;
 
 @CucumberOptions(
         features = "src/test/resources/features",
@@ -23,8 +20,7 @@ public class CucumberAllTestRunner extends AbstractTestNGCucumberTests {
 
     @BeforeSuite
     public void setUp() throws MalformedURLException {
-        WebSteps.driver = new RemoteWebDriver(
-                new URL("http://selenoid-selenoid-1:4444/wd/hub"), remoteChrome());
+        WebSteps.driver = WebDriverFactory.getDriver();
     }
 
     @AfterSuite
@@ -32,14 +28,5 @@ public class CucumberAllTestRunner extends AbstractTestNGCucumberTests {
         WebSteps.driver.quit();
     }
 
-    private ChromeOptions remoteChrome() {
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.setAcceptInsecureCerts(true);
-        chromeOptions.addArguments("--remote-allow-origins=*");
-        chromeOptions.setCapability("selenoid:options", new HashMap<String, Object>() {{
-            put("enableVideo", true);
-            put("enableVNC", true);
-        }});
-        return chromeOptions;
-    }
+
 }
